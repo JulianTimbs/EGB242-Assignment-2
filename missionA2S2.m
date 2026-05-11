663ea630b86c828d224f17962897e3dbde7dcf2f
JulianTimbs <timbsjulian@gmail.com>
2026-05-11 20:08:27 +1000

    Remove autosave files from repo

--- missionA2S2.m
+++ missionA2S2.m
@@ -8,3 +8,20 @@
 clear all; close all;
 
 % Begin writing your MATLAB solution below this line.
+%% 2.1
+% Plot comparison between the step input and the step response
+% g_m(t) = 2(1 - e^-0.5t)
+
+% Time vector
+T = 25;
+samples = 1e4;
+timeVec = linspace(0, T, samples + 1);
+timeVec(end) = [];
+
+% Impulse response
+g_m = 2 * (1 - exp(0.5* timeVec));
+
+% Step response -> G_m(s) * 1/s
+g_step = 2 * (timeVec - 2 + 2*exp(-0.5 * timeVec));
+
+
