5baedb5864ce16588920c2dcc880ad7a48dc6d81
JulianTimbs <timbsjulian@gmail.com>
2026-05-13 18:19:01 +1000

    Added plots for step response with gains included

--- missionA2S2.m
+++ missionA2S2.m
@@ -39,7 +39,7 @@ hold on;
 plot(timeVec, inputStep);
 title("Step Response of DC Motor");
 xlabel("Time [s]");
-ylabel("Rotation [rad]");
+ylabel("Rotation [rad]"); %TODO: Put proper ylabel
 legend("Step Response", "Input Step");
 % plot diverges -> inf so motor alone isn't sufficient to control camera
 
@@ -60,7 +60,7 @@ hold on;
 plot(timeVec, inputStep);
 title("Step Response of DC Motor with Potentiometer");
 xlabel("Time [s]");
-ylabel("Rotation [rad]");
+ylabel("Rotation [rad]"); %TODO: Put proper ylabel
 legend("Step Response", "Input Step");
 % System converges now (under-damped)
 
@@ -73,3 +73,46 @@ legend("Step Response", "Input Step");
 
 %% 2.5
 % TF = K_fwd / (s^2 + 0.5s + K_fwd * K_fb)
+
+Kfwd_values = [0.1, 0.2, 0.5, 1, 2];
+Kfb_values = [0.1, 0.2, 0.5, 1, 2];
+
+legendEntries_fb = cell(1, length(Kfb_values));
+
+figure;
+for i = 1:length(Kfb_values)
+    K_fb = Kfb_values(i);
+    num2 = 1;
+    den2 = [1, 0.5, K_fb];
+    F_gain = tf(num2, den2);
+    F_gain_step = lsim(F_gain, inputStep, timeVec);
+    plot(timeVec, F_gain_step);
+    hold on;
+    legendEntries_fb{i} = sprintf('K_{fb} = %.1f', K_fb);
+end
+legend(legendEntries_fb);
+hold off;
+title("Step Response of DC Motor System with Feedback Gain");
+xlabel("Time [s]");
+ylabel("Rotation [rad]"); %TODO: Put proper ylabel
+legend(legendEntries_fb);
+
+legendEntries_fwd = cell(1, length(Kfwd_values));
+
+figure;
+for i = 1:length(Kfwd_values)
+    K_fwd = Kfwd_values(i);
+    num3 = K_fwd;
+    den3 = [1, 0.5, K_fwd];
+    F_gain = tf(num3, den3);
+    F_gain_step = lsim(F_gain, inputStep, timeVec);
+    plot(timeVec, F_gain_step);
+    hold on;
+    legendEntries_fwd{i} = sprintf('K_{fwd} = %.1f', K_fwd);
+end
+legend(legendEntries_fwd);
+hold off;
+title("Step Response of DC Motor System with Forward Gain");
+xlabel("Time [s]");
+ylabel("Rotation [rad]"); %TODO: Put proper ylabel
+legend(legendEntries_fwd);
