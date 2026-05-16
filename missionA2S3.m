437ae2eb67ed4c87a8bda8ddca81fe0c02b9ad3d
JulianTimbs <timbsjulian@gmail.com>
2026-05-16 17:38:16 +1000

    Added 3.1

--- missionA2S3.m
+++ missionA2S3.m
@@ -9,3 +9,21 @@ clear all; close all;
 load DataA2 imagesReceived;
 
 % Begin writing your MATLAB solution below this line.
+
+%% 3.1 
+% Display the first received landing site image that has been received from the communication
+% channel and comment on the image quality.
+
+numRows = 480;
+numCols = 640;
+
+im1_2D = reshape(imagesReceived(1,:), numRows, numCols);
+
+figure;
+imshow(im1_2D);
+
+% Save image 1 matrix as an image file
+%%%%%%%%%% UNCOMMENT TO SAVE %%%%%%%%%%%%%%
+% imwrite(im2D, 'unfilteredImage.png');
+
+%% 3.2
