40618856621adaf57f063dd77d27d3834ae28d32
Azocado <arranabraham@yahoo.com.au>
2026-05-21 17:18:26 +1000

    Update missionA2S3.m

--- missionA2S3.m
+++ missionA2S3.m
@@ -32,46 +32,52 @@ imshow(im1_2D);
 % Visualise the received signal in both the time and frequency domains.
 
 % Time vector
-T = (numRows * numCols) / 1000;
+fs = 1000;
 samples = numRows * numCols;
+T = (samples) / fs;
 
 t = linspace(0, T, samples + 1);
 t(end) = [];
 
+
+% First received image signal
+% Change the row number to change which
+%image is being analysed throughout the code
+im1_signal = imagesReceived(1,:); 
+
+
 % Frequency Vector
-fs = samples / T;
 
 f = linspace(-fs/2, fs/2, samples + 1);
 f(end) = [];
 
 % Fourier tranform of image signal
 % To access each image use image = n -> im_freq(n,:)
-im_freq = fft(imagesReceived, [], 2) / fs;
-
+im1_freq = fft(im1_signal) / samples;
+    
 % Time domain plot of images
 figure;
-for i = 1:4
-    subplot(2, 2, i);
-    plot(t, imagesReceived(i,:));
-    title(sprintf('Time Domain Plot - Image %d', i))
-    xlabel("Time [s]");
-    ylabel("Amplitude");
-end
+plot(t, im1_signal);
+title(sprintf('Time Domain Plot - Image 1'))
+xlabel("Time [s]");
+ylabel("Amplitude");
+
 
 % Frequency domain (mag & phase spectrum) plot of image
-for i = 1:4
-    figure;
-    subplot(2, 1, 1);
-    plot(f, fftshift(abs(im_freq(i,:))));
-    title(sprintf('Magnitude Spectrum - Image %d', i));
-    xlabel("Frequency [Hz]");
-    ylabel("Amplitude");
-    subplot(2, 1, 2);
-    plot(f, unwrap(fftshift(angle(im_freq(i,:)))));
-    title(sprintf('Phase Spectrum - Image %d', i));
-    xlabel("Frequency [Hz]");
-    ylabel("Phase [rad]");
-end
+
+figure;
+subplot(2, 1, 1);
+plot(f, fftshift(abs(im1_freq)));
+title(sprintf('Magnitude Spectrum - Image 1'));
+xlabel("Frequency [Hz]");
+ylabel("Amplitude");
+subplot(2, 1, 2);
+plot(f, unwrap(fftshift(angle(im1_freq))));
+title(sprintf('Phase Spectrum - Image 1'));
+xlabel("Frequency [Hz]");
+ylabel("Phase [rad]");
+xlim([0 300])
+
 
 %% 3.3
 % Looking for low-pass filter ~50 Hz cut-off I think
@@ -85,7 +91,7 @@ C1 = 10e-6;
 C2 = 4.7e-6;
 
 % Passive filter 1 transfer function
-pFilter1_num = [1/R2*C2, 0];
+pFilter1_num = [1/(R2*C2), 0];
 pFilter1_den = [1, (C2*R1 + C1*R1 + R2*C2)/(R2*R1*C1*C2), 1/(R2*R1*C1*C2)];
 
 pFilter1_TF = tf(pFilter1_num, pFilter1_den);
@@ -117,30 +123,30 @@ aFilter2_TF = tf(aFilter2_num, aFilter2_den);
 f_response = linspace(0, fs/2, 2000);
 w_response = 2*pi*f_response;
 
+pFilter1_mag = squeeze(abs(freqresp(pFilter1_TF, w_response)));
+pFilter2_mag = squeeze(abs(freqresp(pFilter2_TF, w_response)));
 aFilter1_mag = squeeze(abs(freqresp(aFilter1_TF, w_response)));
 aFilter2_mag = squeeze(abs(freqresp(aFilter2_TF, w_response)));
 
 figure;
-plot(f_response, aFilter1_mag);
+plot(f_response, pFilter1_mag);
 hold on;
+plot(f_response, pFilter2_mag);
+plot(f_response, aFilter1_mag);
 plot(f_response, aFilter2_mag);
 xlabel('Frequency [Hz]');
 ylabel('Magnitude');
-title('Frequency Response of Active Filters');
-legend('Active Filter 1', 'Active Filter 2');
+title('Frequency Response of Available Filters');
+legend('Passive Filter 1', 'Passive Filter 2', 'Active Filter 1', 'Active Filter 2');
 grid on;
 
 fc_active2 = 125;
-
-im1_signal = imagesReceived(1,:);
-im1_freq = fftshift(fft(im1_signal)) / samples;
 
 
 %% 3.4
 
 % using the chosen filter (active filter 2) to remove the recieved signal 
 
-im1_signal = imagesReceived(1,:).';   % make column vector for lsim
 t_col = t.';                          % make time vector column
 
 % Apply Active Filter 2
